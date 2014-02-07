8e6c04779161b42f0adc9b48d072248afead309f
EricRBruck <eric@Erics-MacBook-Pro-2.local>
2014-02-06 20:05:59 -0800

    Moved to Chapter01 folder

---
+++ Chapter01/CH01_EX2_MixedMessages2.java
@@ -0,0 +1,23 @@
+//Chapter 1
+//Exercise 2
+//Mixed Messages Part 2
+// Output: 00 11 23 36 410
+
+class CH01_EX2_MixedMessages2 {
+	public static void main(String [] args) {
+		int x = 0;
+		int y = 0;
+		while ( x < 5 ) {
+
+			// Altering Code Start
+
+			y = x + y;
+
+			// Altering Code End
+
+			System.out.print(x + "" + y + " " );
+			x = x + 1;
+		}
+		System.out.println("");
+	}
+}
