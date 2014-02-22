31fd30b027f8ebb9b85602a2661cccf175bc8e3f
EricRBruck <eric@unknown20c9d04332b5.gateway.pace.com>
2014-02-21 19:06:20 -0800

    Update

--- Chapter03/CH03_EX1_Hobbits.java
+++ Chapter03/CH03_EX1_Hobbits.java
@@ -1,7 +1,34 @@
-//Removed main method.
-//Main needs to be in a separate class.
-//I have moved it to CH03_EX1_HobbitsTestDrive.java
 class CH03_EX1_Hobbits {
 
 	String name;
+
+	public static void main(String [] args) {
+
+                CH03_EX1_Hobbits [] h = new CH03_EX1_Hobbits[3];
+                int z = 0;
+
+		//Changed:
+		// while (z < 4) {
+		//to
+		// while (z < 3) {
+		//This would cause an out of bounds reference.
+		//The code would try to reference array position 3
+		//Only legal array positions are 0, 1, and 2.
+                while (z < 3) {
+                        h[z] = new CH03_EX1_Hobbits();
+                        h[z].name = "bilbo";
+                        if (z == 1) {
+                                h[z].name = "frodo";
+                        }
+                        if (z == 2) {
+                                h[z].name = "sam";
+                        }
+                        System.out.print(h[z].name + " is a ");
+                        System.out.println("good Hobbit name");
+
+			//Moved to end of while loop.
+                        //If not array position 0 would never be referenced.
+			z = z + 1;
+                }
+        }
 }
